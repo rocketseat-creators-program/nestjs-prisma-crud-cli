6708778e9b10f2c55c905ea43734e8217c119b80
Paulo Salvatore <salvatoregames@gmail.com>
2021-05-17 01:06:20 -0300

    chore: generate resources for `users` and `posts`

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PostsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
